1c7b5fea8f603e928d56639735293fcd243a3e20
joy zhang <joy.xyz@berkeley.edu>
2025-08-18 14:31:09 -0700

    flight page routing

---
+++ frontend/src/components/utils.js
@@ -0,0 +1,170 @@
+import { DateTime } from "luxon";
+
+const stateToAirports = {
+  "Alabama": ["BHM", "HSV", "MGM", "MOB"], // Birmingham, Huntsville, Montgomery, Mobile
+  "Alaska": ["ANC", "FAI", "JNU", "SEA"], // Anchorage, Fairbanks, Juneau, plus Seattle (common hub)
+  "Arizona": ["PHX", "TUS", "SDL"], // Phoenix, Tucson, Scottsdale (private/charter)
+  "Arkansas": ["XNA", "LIT", "FSM"], // Northwest Arkansas, Little Rock, Fort Smith
+  "California": ["LAX", "SFO", "SAN", "SJC", "OAK", "SMF", "BUR", "ONT", "SNA", "LGB"], // Major CA airports
+  "Colorado": ["DEN", "COS", "GJT"], // Denver, Colorado Springs, Grand Junction
+  "Connecticut": ["BDL", "HVN"], // Bradley (Hartford), Tweed New Haven
+  "Delaware": ["ILG"], // Wilmington (small, near Philadelphia)
+  "District of Columbia": ["DCA", "IAD", "BWI"], // Reagan, Dulles, Baltimore-Washington
+  "Florida": ["MIA", "FLL", "TPA", "MCO", "RSW", "JAX", "PBI"], // Miami, Fort Lauderdale, Tampa, Orlando, Southwest Florida, Jacksonville, Palm Beach
+  "Georgia": ["ATL", "SAV", "AGS", "MCN"], // Atlanta, Savannah, Augusta, Macon
+  "Hawaii": ["HNL", "OGG", "KOA", "LIH"], // Honolulu, Maui, Kona, Lihue
+  "Idaho": ["BOI", "IDA"], // Boise, Idaho Falls
+  "Illinois": ["ORD", "MDW", "SPI", "RFD"], // O’Hare, Midway, Springfield, Rockford
+  "Indiana": ["IND", "SBN"], // Indianapolis, South Bend
+  "Iowa": ["DSM", "CID", "MLI"], // Des Moines, Cedar Rapids, Moline
+  "Kansas": ["ICT", "MCI", "FOE"], // Wichita, Kansas City, Topeka
+  "Kentucky": ["CVG", "SDF", "LEX"], // Cincinnati/Northern KY, Louisville, Lexington
+  "Louisiana": ["MSY", "BTR", "LFT"], // New Orleans, Baton Rouge, Lafayette
+  "Maine": ["PWM", "BGR"], // Portland, Bangor
+  "Maryland": ["BWI", "MTN"], // Baltimore-Washington, Martin State
+  "Massachusetts": ["BOS", "ORH", "EWB"], // Boston Logan, Worcester, New Bedford
+  "Michigan": ["DTW", "GRR", "MBS", "AZO"], // Detroit, Grand Rapids, Saginaw, Kalamazoo
+  "Minnesota": ["MSP", "DLH", "RST"], // Minneapolis-St Paul, Duluth, Rochester
+  "Mississippi": ["JAN", "GPT"], // Jackson, Gulfport
+  "Missouri": ["STL", "MCI", "SGF"], // St. Louis, Kansas City, Springfield
+  "Montana": ["BIL", "GTF", "MSO"], // Billings, Great Falls, Missoula
+  "Nebraska": ["OMA", "LNK"], // Omaha, Lincoln
+  "Nevada": ["LAS", "RNO"], // Las Vegas, Reno
+  "New Hampshire": ["MHT", "CON"], // Manchester, Concord (small)
+  "New Jersey": ["EWR", "ACY", "TTN"], // Newark, Atlantic City, Trenton-Mercer
+  "New Mexico": ["ABQ", "SRR"], // Albuquerque, Santa Rosa (small)
+  "New York": ["JFK", "LGA", "EWR", "BUF", "ROC", "SYR", "ALB"], // JFK, LaGuardia, Newark, Buffalo, Rochester, Syracuse, Albany
+  "North Carolina": ["CLT", "RDU", "GSO", "ILM"], // Charlotte, Raleigh-Durham, Greensboro, Wilmington
+  "North Dakota": ["FAR", "GFK"], // Fargo, Grand Forks
+  "Ohio": ["CLE", "CMH", "CVG", "DAY"], // Cleveland, Columbus, Cincinnati, Dayton
+  "Oklahoma": ["OKC", "TUL"], // Oklahoma City, Tulsa
+  "Oregon": ["PDX", "EUG"], // Portland, Eugene
+  "Pennsylvania": ["PHL", "PIT", "AVP", "MDT"], // Philadelphia, Pittsburgh, Wilkes-Barre/Scranton, Harrisburg
+  "Rhode Island": ["PVD"], // Providence
+  "South Carolina": ["CHS", "GSP", "CAE"], // Charleston, Greenville-Spartanburg, Columbia
+  "South Dakota": ["FSD", "RAP"], // Sioux Falls, Rapid City
+  "Tennessee": ["BNA", "MEM", "CHA"], // Nashville, Memphis, Chattanooga
+  "Texas": ["DFW", "IAH", "AUS", "SAT", "DAL", "HOU"], // Dallas-Fort Worth, Houston, Austin, San Antonio, Dallas Love, Houston Hobby
+  "Utah": ["SLC", "PVU"], // Salt Lake City, Provo (small)
+  "Vermont": ["BTV"], // Burlington
+  "Virginia": ["DCA", "ORF", "RIC"], // Reagan, Norfolk, Richmond
+  "Washington": ["SEA", "GEG", "PDX"], // Seattle, Spokane, Portland (nearby)
+  "West Virginia": ["CRW", "HTS"], // Charleston, Huntington
+  "Wisconsin": ["MKE", "MSN", "GRB"], // Milwaukee, Madison, Green Bay
+  "Wyoming": ["JAC"], // Jackson Hole
+};
+
+const airlineNames = {
+  "6X": "Amadeus Six",
+  "PR": "Philippine Airlines",
+  "AA": "American Airlines",
+  "DL": "Delta Air Lines",
+  "UA": "United Airlines",
+  "WN": "Southwest Airlines",
+  "AS": "Alaska Airlines",
+  "B6": "JetBlue Airways",
+  "AF": "Air France",
+  "LH": "Lufthansa",
+  "EK": "Emirates",
+  "AC": "Air Canada",
+  "BA": "British Airways",
+  "CX": "Cathay Pacific",
+  "QF": "Qantas",
+  "SQ": "Singapore Airlines",
+  "JL": "Japan Airlines",
+  "NH": "All Nippon Airways",
+  "IB": "Iberia",
+  "AZ": "Alitalia",
+  "KL": "KLM Royal Dutch Airlines",
+  "TK": "Turkish Airlines",
+  "SA": "South African Airways",
+  "NZ": "Air New Zealand",
+  "EY": "Etihad Airways",
+  "QR": "Qatar Airways",
+  "MS": "EgyptAir",
+  "CX": "Cathay Pacific",
+  "FI": "Icelandair",
+  "DL": "Delta Air Lines",
+  "F9": "Frontier Airlines",
+  "G4": "Allegiant Air",
+  "HA": "Hawaiian Airlines",
+  "VX": "Virgin America",
+  "WN": "Southwest Airlines",
+  "YV": "Mesa Airlines",
+};
+
+const formatWithTimezone = (dateTimeStr, state) => {
+  const stateTimezones = {
+    "Alabama": "America/Chicago",
+    "Alaska": "America/Anchorage",
+    "Arizona": "America/Phoenix",
+    "Arkansas": "America/Chicago",
+    "California": "America/Los_Angeles",
+    "Colorado": "America/Denver",
+    "Connecticut": "America/New_York",
+    "Delaware": "America/New_York",
+    "District of Columbia": "America/New_York",
+    "Florida": "America/New_York", // see note below
+    "Georgia": "America/New_York",
+    "Hawaii": "Pacific/Honolulu",
+    "Idaho": "America/Boise", // mostly Mountain Time
+    "Illinois": "America/Chicago",
+    "Indiana": "America/Indiana/Indianapolis", // mostly Eastern
+    "Iowa": "America/Chicago",
+    "Kansas": "America/Chicago", // some parts Mountain Time, but mostly Chicago
+    "Kentucky": "America/New_York", // mostly Eastern
+    "Louisiana": "America/Chicago",
+    "Maine": "America/New_York",
+    "Maryland": "America/New_York",
+    "Massachusetts": "America/New_York",
+    "Michigan": "America/Detroit", // Eastern time
+    "Minnesota": "America/Chicago",
+    "Mississippi": "America/Chicago",
+    "Missouri": "America/Chicago",
+    "Montana": "America/Denver",
+    "Nebraska": "America/Chicago", // parts in Mountain Time, but mostly Chicago
+    "Nevada": "America/Los_Angeles", // parts in Mountain Time
+    "New Hampshire": "America/New_York",
+    "New Jersey": "America/New_York",
+    "New Mexico": "America/Denver",
+    "New York": "America/New_York",
+    "North Carolina": "America/New_York",
+    "North Dakota": "America/Chicago", // parts Mountain Time but mostly Chicago
+    "Ohio": "America/New_York",
+    "Oklahoma": "America/Chicago",
+    "Oregon": "America/Los_Angeles", // some parts in Mountain Time
+    "Pennsylvania": "America/New_York",
+    "Rhode Island": "America/New_York",
+    "South Carolina": "America/New_York",
+    "South Dakota": "America/Chicago", // parts Mountain Time but mostly Chicago
+    "Tennessee": "America/Chicago", // mostly Central Time (East Tennessee is Eastern)
+    "Texas": "America/Chicago", // mostly Central, but West Texas is Mountain Time
+    "Utah": "America/Denver",
+    "Vermont": "America/New_York",
+    "Virginia": "America/New_York",
+    "Washington": "America/Los_Angeles",
+    "West Virginia": "America/New_York",
+    "Wisconsin": "America/Chicago",
+    "Wyoming": "America/Denver"
+  };
+
+  const timezone = stateTimezones[state] || "UTC";
+  return DateTime.fromISO(dateTimeStr) 
+    .setZone(timezone)
+    .toFormat("MMM dd, yyyy hh:mm a ZZZZ");
+};
+
+
+const airportToState = {};
+          Object.entries(stateToAirports).forEach(([state, airports]) => {
+            airports.forEach(code => { airportToState[code] = state; });
+          });
+
+
+
+export {
+    stateToAirports,
+    airlineNames,
+    formatWithTimezone,
+    airportToState
+};
